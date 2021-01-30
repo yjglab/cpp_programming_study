f865e595c63276229960d9037d111659d158249a
yjglab <yjgdesign@gmail.com>
2021-01-31 02:03:20 +0900

    관계연산자 Relational Operator

--- B1_11/Source.cpp
+++ B1_11/Source.cpp
@@ -33,12 +33,12 @@ int main() {
 
 	cout << q << endl;
 
-	// sizeof() operator
+	// *** sizeof() operator *** 
 	cout << sizeof(float) << endl; // 4 (32bit)
 	float f;
 	cout << sizeof f << endl; // 4
 
-	// comma operator
+	// *** comma operator *** 
 	int c = 3;
 	int d = 10;
 	int r = (++c, ++d); // 왼쪽계산(++c) => 오른쪽계산(++d) => 오른쪽값을 r에 할당
@@ -51,13 +51,50 @@ int main() {
 	e = (w, u);
 	cout << e << endl; // 10
 
-	// 조건부 연산자 conditional operator ( == arithmetric if)
+	// *** 조건부 연산자 conditional operator ( == arithmetric if) *** 
 	bool onSale = true;
 	const int price = (onSale) ? 20 : 30;
 	cout << price << endl; // 20
 	
 	// 연산자 사용 중 오류가 나는 경우 연산순위를 생각하기
-	cout << ((5 % 2 == 0) ? "even" : "odd");
+	cout << ((5 % 2 == 0) ? "even" : "odd") << endl;
+	
+	// *** 관계 연산자 Relational operator *** 
+	int r1, r2;
+	cin >> r1 >> r2;
+	if (r1 == r2)
+		cout << "equal" << endl;
+	if (r1 != r2)
+		cout << "Not equal" << endl;
+	if (r1 > r2)
+		cout << "r1 is greater than r2" << endl;
+	if (r1 < r2)
+		cout << "r1 is less than r2" << endl;
+	if (r1 >= r2)
+		cout << "r1 is greater than r2 or equal to r2" << endl;
+	if (r1 <= r2)
+		cout << "r1 is less than r2 or equal to r2" << endl;
 
+	double d1(100 - 99.99);
+	double d2(10 - 9.99);
+	cout << d1 << endl;
+	cout << d2 << endl;
+	if (d1 == d2)
+		cout << "equal" << endl;
+	else {
+		cout << "Not equal" << endl; // 실행
+		if (d1 > d2)
+			cout << "d1 > d2" << endl; // 실행
+		else {
+			cout << "d1 < d2" << endl;
+		}
+	}
+	const double epsilon = 1e-10; // 차이를 판별하기 위한 기준 설정
+	if (std::abs(d1 - d2) < epsilon)
+		cout << "Approximately equal" << endl; // 실행
+	else
+		cout << "Not equal, never" << endl;
+
+	cout << std::abs(d1 - d2) << endl; // 5.32907e-15
 	return 0;
 }
