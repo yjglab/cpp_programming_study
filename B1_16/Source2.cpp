f4733ec7030b56b076d95c5286ca81c9b3351342
yjglab <yjgdesign@gmail.com>
2021-02-03 02:55:41 +0900

    Type aliases

--- B1_16/Source2.cpp
+++ B1_16/Source2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main4() {
 	enum class Color {
 		RED,
 		BLUE,
---
+++ B1_16/Source3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() {
+	// *** 자료형에 가명 붙이기 Type aliases ***
+	typedef double distance_t; // distance_t 라는 가명의 자료형을 쓸때 double을 사용하겠음
+
+	double myDistance = 1.0;
+	distance_t myDistance2 = 2.0;
+
+	// 사용 사례1
+	typedef vector<pair<string, int> > pairlist_t;
+	pairlist_t pairlist1;
+	pairlist_t pairlist2;
+
+	// 사용 사례2
+	using pairList_t = vector<pair<string, int> >;
+	pairList_t pairList1;
+	pairList_t pairList2;
+	
+
+	return 0;
+}
