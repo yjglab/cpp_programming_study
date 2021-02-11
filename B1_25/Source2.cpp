fa0d22329ffd11fc13005b9c0aeaa273143de2ff
yjglab <yjgdesign@gmail.com>
2021-02-11 21:30:41 +0900

    재귀적 함수 호출 Recursive Function Call

---
+++ B1_25/Source2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+// *** 재귀적 함수 호출 Recursive Function Call ***
+
+void countDown(int count) {
+	cout << count << endl;
+	if (count > 0) 
+		countDown(count - 1);
+	
+}
+int sumTo(int sumto) {
+	if (sumto <= 0) {
+		return 0;
+	}
+	else if (sumto <= 1) {
+		return 1;
+	}
+	else {
+		return sumTo(sumto - 1) + sumto;
+	}
+}
+int main() {
+	countDown(5); // 5 4 3 2 1 0
+	
+	cout << sumTo(10) << endl; // 55
+	return 0;
+}
