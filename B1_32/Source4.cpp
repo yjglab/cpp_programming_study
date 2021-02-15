088fbb1b1ea8eaab0cae8153278afd677ae90502
yjglab <yjgdesign@gmail.com>
2021-02-16 01:43:50 +0900

    클래스 안에 포함된 자료형 nested types

--- B1_32/Source4.cpp
+++ B1_32/Source4.cpp
@@ -1,3 +1,37 @@
 #include <iostream>
 
 using namespace std;
+
+// *** 클래스 안에 포함된 자료형 nested types ***
+
+class Fruit {
+public:
+	enum class FruitType {
+		APPLE, BANANA, CHERRY,
+	};
+	class InnerClass {
+		// ...
+	};
+	struct InnerStruct {
+		// ...
+	};
+private:
+	FruitType _type;
+
+public:
+	Fruit(FruitType type)
+		: _type(type)
+	{
+
+	}
+	FruitType getType() {
+		return _type;
+	}
+};
+int main6() {
+	Fruit apple(Fruit::FruitType::APPLE);
+	if (apple.getType() == Fruit::FruitType::APPLE) {
+		cout << "It is Apple" << endl;
+	}
+	return 0;
+}
