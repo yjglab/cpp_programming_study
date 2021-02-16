2bb10f52c5c0c5a24cf4a287943f54cfb288a5b5
yjglab <yjgdesign@gmail.com>
2021-02-17 02:23:48 +0900

    괄호 연산자 () 오버로딩과 함수 객체

---
+++ B1_33/Source6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// *** 괄호 연산자 () 오버로딩과 함수 객체 ***
+
+class Accumulator {
+private:
+	int _counter = 0;
+
+public:
+	int operator()(int i) {
+		return (_counter += i);
+	}
+};
+
+int main8() {
+
+	Accumulator acc;
+	cout << acc(10) << endl; // 10
+	cout << acc(20) << endl; // 30
+
+	return 0;
+}
